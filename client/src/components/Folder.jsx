897dea543486c7b9218c2b525a69ce43b4623ec4
TalyaAlbeck <talyaalbeck@hilma.tech>
2024-11-20 21:24:26 +0200

    try to get the folders in fetch

--- client/src/components/Folder.jsx
+++ client/src/components/Folder.jsx
@@ -5,20 +5,27 @@ import { useState } from 'react'
 
 export default function Folder({userData}) {
     const [filderName, setFolderName] = useState('')
+    const [showBody, setShowBody] = useState(undefined)
     // const [index, setIndex] = useState()
     console.log(userData);
-    function openFolder(item) {
+    function openFolder(item, index) {
         console.log(item);
-        setFolderName(item)
-        
-        
+        setFolderName(item) 
+        showBody === index ? setShowBody(undefined) : setShowBody(index)          
     }
+
+    // useEffect(async () => {
+    //   const data = await fetch(`http://localhost:3000/folder/`)
+    // }, [])
     
   return (
     <>
     <p>the path is: folders // {filderName.name}</p>
     {userData.map((item, index) => {
-        return <div className='foldersDivs' key={index} onDoubleClick={() => openFolder(item)}>{(item.name)}</div>
+        return (<>
+        <div className='foldersDivs' key={index} onDoubleClick={() => openFolder(item, index)}>{(item.name)}</div>
+        {showBody === index && <div className='folderBodyDiv'>{item.body}</div>}
+        </>)
     })}
     </>
   )
