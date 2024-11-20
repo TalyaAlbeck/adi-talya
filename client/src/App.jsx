dd1edfbde9fcc32419d6874b8159f635845a9aab
TalyaAlbeck <talyaalbeck@hilma.tech>
2024-11-20 12:04:53 +0200

    open the files + routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './App.css'
+import Login from './components/Login'
 
 function App() {
 
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="*" element={<h1>404 not found</h1>} />
+      </Routes>
+    </Router>
       
     </>
   )
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Login() {
+    
+  return (
+    <form>
+        <h1>nini</h1>
+    </form>
+  )
+}
