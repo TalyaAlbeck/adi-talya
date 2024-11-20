87d61d521ba2075539bcb5221cad2dc9283dbf2a
TalyaAlbeck <talyaalbeck@hilma.tech>
2024-11-20 18:16:20 +0200

    get the files content

--- server/folderHandler.js
+++ server/folderHandler.js
@@ -2,8 +2,12 @@ const fs = require('fs');
 
 exports.ls = async function (name) {
     const files = await fs.promises.readdir(`folders/${name}`);
-
-    return files;
+    const filesArr = [];
+    for (let file of files) {
+        const fileContent = await fs.promises.readFile(`folders/${name}/${file}`, { encoding: 'utf-8' })
+        filesArr.push(fileContent)
+    }
+    return filesArr;
 
 }
 
