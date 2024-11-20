249192fd8b5161e6b818cc1f9fd3428482c4f7f7
TalyaAlbeck <talyaalbeck@hilma.tech>
2024-11-20 13:03:24 +0200

    ls fonction

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
+
+function ls() {
+
+  fs.readdir("folders", (err, res) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log(res);
+      for (let i = 0; i < res.length; i++) {
+        fs.stat(`./folders/${res[i]}`, (err, stats) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          // stats.isFile(); // true
+          if (stats.isFile()) { console.log(res[i] + " is a file") }
+          else console.log(res[i] + " is a folder")
+        })
+
+      }
+    }
+
+  })
+}
+
+
+router.post('/', (req, res) => {
+  ls()
+})
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+// ls()
 module.exports = router;
